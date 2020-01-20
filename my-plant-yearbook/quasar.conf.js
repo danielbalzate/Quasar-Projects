cad4e1e37afd60f8a0e21640b11c8665b2ffb40d
Daniel B <danielb.alzate1@gmail.com>
2020-01-20 07:50:00 -0500

    Start my plant yearbook implementation

--- my-plant-yearbook/quasar.conf.js
+++ my-plant-yearbook/quasar.conf.js
@@ -1,19 +1,15 @@
 // Configuration for your app
 // https://quasar.dev/quasar-cli/quasar-conf-js
 
-module.exports = function (ctx) {
+module.exports = function(ctx) {
   return {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/cli-documentation/boot-files
-    boot: [
-      'axios'
-    ],
+    boot: ["axios", "firebase"],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
-    css: [
-      'app.scss'
-    ],
+    css: ["app.scss"],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
@@ -24,14 +20,14 @@ module.exports = function (ctx) {
       // 'themify',
       // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
 
-      'roboto-font', // optional, you are not bound to it
-      'material-icons' // optional, you are not bound to it
+      "roboto-font", // optional, you are not bound to it
+      "material-icons" // optional, you are not bound to it
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
     framework: {
-      iconSet: 'material-icons', // Quasar icon set
-      lang: 'en-us', // Quasar language pack
+      iconSet: "material-icons", // Quasar icon set
+      lang: "en-us", // Quasar language pack
 
       // Possible values for "all":
       // * 'auto' - Auto-import needed Quasar components & directives
@@ -40,13 +36,13 @@ module.exports = function (ctx) {
       //            (fastest compile time; minimum bundle size; most tedious)
       // * true   - Import everything from Quasar
       //            (not treeshaking Quasar; biggest bundle size; convenient)
-      all: 'auto',
+      all: "auto",
 
-      components: [],
+      components: ["QEditor"],
       directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ["Notify", "Dialog"]
     },
 
     // https://quasar.dev/quasar-cli/cli-documentation/supporting-ie
@@ -55,7 +51,7 @@ module.exports = function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       scopeHoisting: true,
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: "hash", // available values: 'hash', 'history'
       showProgress: true,
       gzip: false,
       analyze: false,
@@ -64,8 +60,7 @@ module.exports = function (ctx) {
       // extractCSS: false,
 
       // https://quasar.dev/quasar-cli/cli-documentation/handling-webpack
-      extendWebpack (cfg) {
-      }
+      extendWebpack(cfg) {}
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
@@ -86,41 +81,41 @@ module.exports = function (ctx) {
 
     // https://quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
-      workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
+      workboxPluginMode: "GenerateSW", // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: 'Quasar App',
-        short_name: 'Quasar App',
-        description: 'A Quasar Framework app',
-        display: 'standalone',
-        orientation: 'portrait',
-        background_color: '#ffffff',
-        theme_color: '#027be3',
+        name: "Quasar App",
+        short_name: "Quasar App",
+        description: "A Quasar Framework app",
+        display: "standalone",
+        orientation: "portrait",
+        background_color: "#ffffff",
+        theme_color: "#027be3",
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
-            'sizes': '128x128',
-            'type': 'image/png'
+            src: "statics/icons/icon-128x128.png",
+            sizes: "128x128",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-192x192.png',
-            'sizes': '192x192',
-            'type': 'image/png'
+            src: "statics/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-256x256.png',
-            'sizes': '256x256',
-            'type': 'image/png'
+            src: "statics/icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-384x384.png',
-            'sizes': '384x384',
-            'type': 'image/png'
+            src: "statics/icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png"
           },
           {
-            'src': 'statics/icons/icon-512x512.png',
-            'sizes': '512x512',
-            'type': 'image/png'
+            src: "statics/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
           }
         ]
       }
@@ -129,9 +124,8 @@ module.exports = function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
     cordova: {
       // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
-      id: ''
+      id: ""
     },
-
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
     capacitor: {
@@ -140,17 +134,15 @@ module.exports = function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-electron-apps/configuring-electron
     electron: {
-      bundler: 'packager', // 'packager' or 'builder'
+      bundler: "packager", // 'packager' or 'builder'
 
       packager: {
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
-
         // OS X / Mac App Store
         // appBundleId: '',
         // appCategoryType: '',
         // osxSign: '',
         // protocol: 'myapp://path',
-
         // Windows only
         // win32metadata: { ... }
       },
@@ -158,16 +150,16 @@ module.exports = function (ctx) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'my-plant-yearbook'
+        appId: "my-plant-yearbook"
       },
 
       // More info: https://quasar.dev/quasar-cli/developing-electron-apps/node-integration
       nodeIntegration: true,
 
-      extendWebpack (cfg) {
+      extendWebpack(cfg) {
         // do something with Electron main process Webpack cfg
         // chainWebpack also available besides this extendWebpack
       }
     }
-  }
-}
+  };
+};
--- my-plant-yearbook/src/pages/Index.vue
+++ my-plant-yearbook/src/pages/Index.vue
@@ -1,11 +1,204 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
-  </q-page>
-</template>
+  <div class="q-pa-md q-gutter-sm">
+    <!-- <q-editor
+      v-if="modoEdicion"
+      v-model="editor"
+      :definitions="{
+        save: {
+          tip: 'Actualizar descripción',
+          icon: 'save',
+          label: 'Actualizar',
+          handler: updateWork
+        }
+      }"
+      :toolbar="[
+        ['bold', 'italic', 'strike', 'underline'],
+        ['upload', 'save']
+      ]"
+    />
 
+    <q-editor
+      v-else
+      v-model="editor"
+      :definitions="{
+        save: {
+          tip: 'Guardar descripción',
+          icon: 'save',
+          label: 'Guardar',
+          handler: saveWork
+        }
+      }"
+      :toolbar="[
+        ['bold', 'italic', 'strike', 'underline'],
+        ['upload', 'save']
+      ]"
+    /> -->
+
+    <q-card
+      class="row"
+      flat
+      bordered
+      v-for="(item, index) in plants"
+      :key="index"
+    >
+      <q-card-section
+        class="col"
+        v-html="item.text"
+        :class="item.status ? 'tachar' : ''"
+      />
+      <!-- <q-btn flat color="blue" @click="item.status = !item.status"
+        >Acción</q-btn
+      > -->
+      <q-btn flat color="yellow" @click="editPlant(index, item.id)"
+        >Editar</q-btn
+      >
+      <q-btn flat color="red" @click="deletePlant(index, item.id)"
+        >Eliminar</q-btn
+      >
+    </q-card>
+    <div class="flex flex-center" v-if="plants.length == 0">
+      <h6>Sin notas</h6>
+    </div>
+  </div>
+</template>
 <script>
+import { db } from "boot/firebase";
 export default {
-  name: 'PageIndex'
-}
+  data() {
+    return {
+      editor: "",
+      plants: [
+        /*  {
+          id: "asdasd",
+          text: "asdasd",
+          status: false
+        } */
+      ],
+      index: null,
+      modoEdicion: false,
+      id: null
+    };
+  },
+  created() {
+    this.listPlants();
+  },
+  methods: {
+    async updateWork() {
+      try {
+        const resDB = await db
+          .collection("plants")
+          .doc(this.id)
+          .update({
+            text: this.editor
+          });
+        this.plants[this.index].text = this.editor;
+        this.$q.notify({
+          message: "Se ha actualizado correctamente",
+          color: "green-4",
+          textColor: "white",
+          icon: "check"
+        });
+      } catch (error) {
+        console.log("TCL: updateWork -> error", error);
+      } finally {
+        this.modoEdicion = false;
+        this.index = null;
+        this.id = null;
+        this.editor = "";
+      }
+    },
+    editPlant(index, id) {
+      this.modoEdicion = true;
+      this.index = index;
+      this.id = id;
+      this.editor = this.plants[index].text;
+    },
+    async listPlants() {
+      try {
+        const redDB = await db.collection("plants").get();
+        redDB.forEach(res => {
+          const plant = {
+            id: res.id,
+            text: res.data().text,
+            status: res.data().status
+          };
+          this.plants.push(plant);
+        });
+      } catch (error) {
+        console.log("TCL: listPlants -> error", error);
+      }
+    },
+    async saveWork() {
+      try {
+        const resDB = await db.collection("plants").add({
+          text: this.editor,
+          status: false
+        });
+        this.plants.push({
+          id: resDB.id,
+          text: this.editor,
+          status: false
+        });
+        this.editor = "";
+        this.$q.notify({
+          message: "Se ha guardado correctamente",
+          color: "green-4",
+          textColor: "white",
+          icon: "cloud_done"
+        });
+      } catch (error) {
+        console.log("TCL: saveWork -> error", error);
+      }
+    },
+    uploadIt() {
+      this.$q.notify({
+        message: "Server unavailable. Check connectivity.",
+        color: "red-5",
+        textColor: "white",
+        icon: "warning"
+      });
+    },
+    deletePlant(index, id) {
+      this.$q
+        .dialog({
+          title: "Acción peligrosa",
+          message: "¿Realmente quieres eliminar la planta?",
+          cancel: true,
+          persistent: true
+        })
+        .onOk(async () => {
+          try {
+            await db
+              .collection("plants")
+              .doc(id)
+              .delete();
+          } catch (error) {
+            console.log("TCL: deletePlant -> error", error);
+          }
+          this.plants.splice(index, 1);
+          this.$q.notify({
+            message: "Se ha eliminado correctamente",
+            color: "red-4",
+            textColor: "white",
+            icon: "info"
+          });
+          // console.log('>>>> OK')
+        })
+        .onOk(() => {
+          // console.log('>>>> second OK catcher')
+        })
+        .onCancel(() => {
+          // console.log('>>>> Cancel')
+        })
+        .onDismiss(() => {
+          // console.log('I am triggered on both OK and Cancel')
+        });
+    }
+  }
+};
 </script>
+<style>
+.tachar {
+  text-decoration: line-through;
+}
+</style>
